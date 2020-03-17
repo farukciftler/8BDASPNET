314365ae3a0f42a431372be9583b38487d2f2394
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2020-03-17 16:32:26 +0300

    regex updated

--- 8BD/Helpers/RegexHelper.cs
+++ 8BD/Helpers/RegexHelper.cs
@@ -8,17 +8,26 @@ namespace _8BD.Helpers
 {
     public class RegexHelper
     {
-        public String[] patterns = new String[2]
+        public String[] patterns = new String[6]
          {
              @"\(b: (\s?)*(?<bold>(.*?))\)",
-             @"\(i: (\s?)*(?<italic>(.*?))\)"
+             @"\(i: (\s?)*(?<italic>(.*?))\)",
+             @"\(bkz: (\s?)*(?<bkz>(.*?))\)",
+             @"\(gbkz: (\s?)*(?<gbkz>(.*?))\)",
+             @"(?:https?:)?(?:\/\/)?(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\/\S*?[^\w\s-])(?<youtube>((?!videoseries)[\w-]{11}))(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['""][^<>]*>|<\/a>))[?=&+%\w.-]*",
+             @"(?<image>^(?:(?<scheme>[^:\/?#]+):)?(?:\/\/(?<authority>[^\/?#]*))?(?<path>[^?#]*\/)?(?<file>[^?#]*\.(?<extension>[Jj][Pp][Ee]?[Gg]|[Pp][Nn][Gg]|[Gg][Ii][Ff]))(?:\?(?<query>[^#]*))?(?:#(?<fragment>.*))?$)"
+
 
 
          };
-        public String[] replacements = new String[2] 
+        public String[] replacements = new String[6]
         {
         @"<b>${bold}</b>",
-        @"<i>${italic}</i>"
+        @"<i>${italic}</i>",
+        @"(bkz: <a href='subject?search=${bkz}'>${bkz}</a>)",
+        @"<a href='subject?search=${gbkz}'>${gbkz}</a>",
+        @"</p><iframe style='width:99%; height: 400px;' src='https://www.youtube.com/embed/${youtube}' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe><p class='tanim'>",
+        @"<img style='width:99%; height: 400px;' src='${image}'>"
         };
 
 
