66c179c74e7ac81124d8d5f3896ace083ae8c2de
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2020-07-03 08:20:29 +0300

    announcement added

--- 8BD/Controllers/AnnouncementController.cs
+++ 8BD/Controllers/AnnouncementController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _8BD.Helpers;
+using _8BD.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,12 +11,36 @@ namespace _8BD.Controllers
 {
     public class AnnouncementController : Controller
     {
+
+        private readonly HttpHelper _helper;
+
+        public AnnouncementController(HttpHelper helper)
+        {
+            _helper = helper;
+        }
+
         [HttpGet("announcements")]
         public IActionResult Index()
         {
             ViewBag.Name = HttpContext.Session.GetString("_username");
             ViewBag.Pass = HttpContext.Session.GetString("_password");
+            ViewBag.Level = HttpContext.Session.GetInt32("_level");
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(string announce)
+        {
+
+            Announcement announcement = new Announcement();
+            announcement.announcement = announce;
+            announcement.userId= (int)HttpContext.Session.GetInt32("_id");
+            var apiendpoint = $"/announcements";
+            _helper.PostMethod<Announcement>(announcement, apiendpoint);
+            ViewBag.Name = HttpContext.Session.GetString("_username");
+            ViewBag.Pass = HttpContext.Session.GetString("_password");
+            ViewBag.Level = HttpContext.Session.GetInt32("_level");
+            return Redirect("/announcements");
         }
     }
 }
---
+++ 8BD/Models/Announcement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _8BD.Models
+{
+    public class Announcement
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public string announcement { get; set; }
+        public DateTime createDate { get; set; }
+
+        public Announcement()
+        {
+            announcement = "";
+            createDate = DateTime.Now;
+        }
+    }
+}
